a07f919c614e30b5cd81d0bc869c6bb9a9013bc4
Dmytro Orikhovskyi <dimaoag@gmail.com>
2018-01-25 10:38:28 +0200

    connection with server

--- config/db_params.php
+++ config/db_params.php
@@ -1,4 +1,13 @@
 <?php
+
+//return array(
+//    'host' => 'vpsgroun.mysql.tools',
+//    'dbname' => 'vpsgroun_dbser',
+//    'user' => 'vpsgroun_dbser',
+//    'password' => '123456',
+//);
+
+
 return array(
   'host' => 'phpstart.loc',
   'dbname' => 'db_1',
